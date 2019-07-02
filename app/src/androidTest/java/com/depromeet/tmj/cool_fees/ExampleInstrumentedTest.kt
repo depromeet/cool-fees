bfeebc4a1c10885da188865e4b30daf82aa99069
Hyunsik-Yoo <dbgustlr92@gmail.com>
2019-07-02 18:05:18 +0900

    캘린더 라이브러리 추가

--- app/src/androidTest/java/com/depromeet/tmj/cool_fees/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/depromeet/tmj/cool_fees/ExampleInstrumentedTest.kt
@@ -1,7 +1,8 @@
 package com.depromeet.tmj.cool_fees
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
